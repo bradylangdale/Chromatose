09b966b7ed4d30991efd7e4c842f4c42360f1c9d
Brady Langdale <bradylangdale@gmail.com>
2023-03-27 01:19:52 -0500

    Fix error

--- panda3dExperiments/test1.py
+++ panda3dExperiments/test1.py
@@ -5,7 +5,5 @@ class MyApp(ShowBase):
     def __init__(self):
         ShowBase.__init__(self)
 
-        self.scene = self.loader.loadModel("Assets/assets/Map/Map.gltf")
-
 app = MyApp()
 app.run()
